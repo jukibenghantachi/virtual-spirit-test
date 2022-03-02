93284faedc64575d4cdc8c45de95181f09a4a6ac
Juki <jukibenghantachi@gmail.com>
2022-03-03 05:33:55 +0700

    feat: post components

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getData } from './redux/actions';
 import CardList from './components/CardList';
+import CardPost from './components/CardPost';
 
 export default function App() {
   const dispatch = useDispatch();
   const state = useSelector((state) => state.reducers);
+  const [forms, setForms] = useState({
+    title: '',
+    description: '',
+    userId: 1,
+  });
+
+  const onSubmitChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setForms({ ...forms, [name]: value });
+  };
+
+  const onSubmitClick = () => {
+    if (forms.title === '' || forms.description === '') {
+      return alert('Please fill the forms!');
+    }
+    dispatch(postData({ body: forms }));
+  };
 
   useEffect(() => {
     dispatch(getData());
   }, [dispatch]);
 
   return (
-    <div className="d-flex flex-wrap justify-content-between">
-      {state.data.length >= 0
-        ? state.data.map((items) => (
-            <CardList
-              userId={items.userId}
-              id={items.id}
-              title={items.title}
-              body={items.body}
-            />
-          ))
-        : null}
+    <div>
+      <CardPost
+        forms={forms}
+        onSubmitChange={onSubmitChange}
+        onSubmitClick={onSubmitClick}
+      />
+      <div className="d-flex flex-wrap justify-content-between">
+        {state.data.length >= 0
+          ? state.data.map((items) => (
+              <CardList
+                userId={items.userId}
+                id={items.id}
+                title={items.title}
+                body={items.body}
+              />
+            ))
+          : null}
+      </div>
     </div>
   );
 }
---
+++ src/components/CardPost.jsx
@@ -0,0 +1,36 @@
+import { Button, Container, Form } from 'react-bootstrap';
+
+export default function CardPost({ forms, onSubmitChange, onSubmitClick }) {
+  return (
+    <Container>
+      <Form className="m-3">
+        <Form.Group className="mb-3" controlId="Title">
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            name="title"
+            type="text"
+            placeholder="React Redux"
+            value={forms.title}
+            onChange={onSubmitChange}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="Description">
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            name="description"
+            as="textarea"
+            placeholder="React Redux is state management."
+            rows={3}
+            value={forms.description}
+            onChange={onSubmitChange}
+          />
+        </Form.Group>
+      </Form>
+      <div className="d-flex flex-wrap justify-content-center">
+        <Button variant="success" onClick={onSubmitClick}>
+          Post
+        </Button>
+      </div>
+    </Container>
+  );
+}
