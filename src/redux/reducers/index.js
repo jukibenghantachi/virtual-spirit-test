3758700e85e6dbb120bb108835e4073fab606830
Juki <jukibenghantachi@gmail.com>
2022-03-03 09:16:43 +0700

    chore: fix space code

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -14,11 +14,9 @@ export default function reducers(state = initialState, action) {
     case GET_DATA_SUCCESS:
       initialState.data.push(action.data);
       return { data: initialState.data };
-
     case POST_DATA_SUCCESS:
       initialState.data.unshift(action.data);
       return { data: initialState.data };
-
     case EDIT_DATA_SUCCESS:
       initialState.data.map((items, index) => {
         if (items.id === action.data.id) {
@@ -26,11 +24,9 @@ export default function reducers(state = initialState, action) {
         }
       });
       return { data: initialState.data };
-
     case DELETE_DATA_SUCCESS:
       initialState.data = initialState.data.filter((x) => x.id !== action.data);
       return { data: initialState.data };
-
     default:
       return state;
   }
